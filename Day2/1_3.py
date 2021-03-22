e30ef46c9dfe07eb0692d156cc261cae395ddd2b
chetanadhikary <chetanadhikary@gmail.com>
2021-03-22 19:48:15 +0530

    Day 2 Folder Updated

---
+++ Day2/1_3.py
@@ -0,0 +1,3 @@
+c = float(input('Enter the temperature in Celsius:'))
+f = (c*(9/5))+32
+print('Degree in Farenheit: ',f)
---
+++ Day2/dict_1.py
@@ -0,0 +1,8 @@
+u_id = input('Please enter your ID')
+u_name = input('Please enter your name')
+u_hob = input('Please enter your hobbies')
+dict_user = dict()
+dict_user['ID'] = u_id
+dict_user['Name'] = u_name
+dict_user['Hobbies'] = u_hob
+print(dict_user)
